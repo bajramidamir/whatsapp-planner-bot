73d19a2aea172f6f6e863e1f184293381b667799
Damir Bajrami <bajrami.damir@gmail.com>
2024-06-13 02:33:25 +0200

    new build and start scripts

--- render-start.sh
+++ render-start.sh
@@ -1,2 +1,3 @@
 # be sure to add Chromes location to the PATH as part of your Start Command
-export PATH="${PATH}:/opt/render/project/.render/chrome/opt/google/chrome"
+# export PATH="${PATH}:/opt/render/project/.render/chrome/opt/google/chrome"
+npm run start
