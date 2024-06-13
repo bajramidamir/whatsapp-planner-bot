7b5822f9ce117e9e22012fb4a7ae51797810ef1d
Damir Bajrami <bajrami.damir@gmail.com>
2024-06-13 02:06:27 +0200

    trying to fix lol

--- server.js
+++ server.js
@@ -31,11 +31,19 @@ venom
             console.log('Session: ', session);
         },
         {
-            folderNameToken: 'tokens',
-            mkdirFolderToken: '',
-            headless: true, // Run without opening a browser
-            browserArgs: ['--no-sandbox'] // Additional argument to avoid sandbox issues
-        }
+            folderNameToken: 'tokens', //folder name when saving tokens
+            mkdirFolderToken: '', //folder directory tokens, just inside the venom folder, example:  { mkdirFolderToken: '/node_modules', } //will save the tokens folder in the node_modules directory
+            headless: true, // Headless chrome
+            devtools: false, // Open devtools by default
+            useChrome: true, // If false will use Chromium instance
+            debug: false, // Opens a debug session
+            logQR: true, // Logs QR automatically in terminal
+            browserArgs: ['--no-sandbox', '--disable-setuid-sandbox'], // Parameters to be added into the chrome browser instance
+            disableSpins: true, // Will disable Spinnies animation, useful for containers (docker) for a better log
+            disableWelcome: true, // Will disable the welcoming message which appears in the beginning
+            updates: true, // Logs info updates automatically in terminal
+            autoClose: 60000, // Automatically closes the venom-bot only when scanning the QR code (default 60 seconds, if you want to turn it off, assign 0 or false)
+          }
     )
     .then((client) => start(client))
     .catch((error) => console.error(error));
